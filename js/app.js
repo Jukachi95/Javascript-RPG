a0b775f0cb3a571c95a41ca5fdb8eda746d18bdc
Joel Okoro <jukachi95@gmail.com>
2020-05-29 23:52:09 +0100

    Expanded on largHit comment

--- js/app.js
+++ js/app.js
@@ -197,7 +197,7 @@ let manaMinus = {
   let orbitalWave = {
 
     // Deals serious heavy dmage to enemy to the point where they are almost defeated
-    // Check if method exists first
+    // Check if the large method exists first
 
     largeHit()
   }
