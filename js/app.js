01839108bb163bccc72844782caa07452f628ef0
Joel Okoro <jukachi95@gmail.com>
2020-06-12 23:32:17 +0100

    Instruction to add moves and attacks as properties to classes

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@
 
 // ****Player****
 // 1. 3 player types to choose from -  warrior, mage, archer and widcard - Wildcard mechanism needs to be expanded on
-// 2. Create a list of moves for each player
+// 2. Create a list of moves for each player and add them as properties to class?
 // 3. Depending on what move they choose, subtract the mana and if it is health boost, change status of player
 // 4. If an attack is chosen, subtract health from the boss
 
